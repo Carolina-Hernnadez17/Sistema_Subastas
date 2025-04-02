664786719b0c0af3cb107332329cc92d90fe7d5b
Katherine Gutierrez <katherine.gutierrez@catolica.edu.sv>
2025-04-02 11:48:06 -0600

    Agregando controlador_K y vistas para graficos

---
+++ Sistema_Subastas/Controllers/Grafico_K.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sistema_Subastas.Controllers
+{
+    public class Grafico_K : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult GraficoSubastasMes()
+        {
+            return View();
+        }
+    }
+}
