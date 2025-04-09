30cadfe95943c67a0cecf63d8753de2a8e422273
Rebeca Margarita Cruz Polanco <rebeca.cruz@catolica.edu.sv>
2025-04-09 03:25:44 -0600

    Subiendo cambios en home

--- Sistema_Subastas/Controllers/HomeController.cs
+++ Sistema_Subastas/Controllers/HomeController.cs
@@ -42,6 +42,10 @@ namespace Sistema_Subastas.Controllers
 
             ViewBag.NombreUsuario = nombre;
 
+            MarcarSubastasFinalizadas();
+            await CerrarSubastasFinalizadas();
+            DeterminarGanadores();
+
             // Aquí llamas al método async correctamente
             await CerrarSubastasFinalizadas();
 
@@ -133,6 +137,7 @@ namespace Sistema_Subastas.Controllers
                         string mensaje = $"📢 Tu subasta {subasta.titulo} ha sido vendida  el ganador es el cod " +
                             $"de usuario: {puj.usuario_id} y el monto final es de: {puj.monto}, fecha de subasta finalizada: {subasta.fecha_fin:dd/MM/yyyy}.";
 
+                        string mensaje2 = $"📢 Has sido ganador de la subasta {subasta.titulo} felicidades.";
                         // Verificar si ya se envió esta notificación
                         bool yaExiste = _context.notificaciones.Any(n =>
                             n.usuario_id == subasta.usuario_id &&
@@ -147,10 +152,26 @@ namespace Sistema_Subastas.Controllers
                                 leido = false,
                                 fecha = DateTime.Now
                             };
+                            
 
                             _context.notificaciones.Add(notificacion);
 
                             // await _hubContext.Clients.All.SendAsync("RecibirNotificacion", subasta.usuario_id, mensaje);
+                        }
+                        bool yaExiste2 = _context.notificaciones.Any(n =>
+                            n.usuario_id == puj.usuario_id &&
+                            n.mensaje == mensaje2);
+
+                        if (!yaExiste2) {
+
+                            var notificacion2 = new notificaciones
+                            {
+                                usuario_id = puj.usuario_id,
+                                mensaje = mensaje2,
+                                leido = false,
+                                fecha = DateTime.Now
+                            };
+                            _context.notificaciones.Add(notificacion2);
                         }
                     }
 
@@ -162,6 +183,56 @@ namespace Sistema_Subastas.Controllers
 
             return Ok("Subastas cerradas y notificaciones enviadas.");
         }
+        public void MarcarSubastasFinalizadas()
+        {
+            var articulosParaFinalizar = _context.articulos
+                .Where(a => a.estado_subasta == "Publicado" && a.fecha_fin <= DateTime.Now)
+                .ToList();
+
+            foreach (var articulo in articulosParaFinalizar)
+            {
+                articulo.estado_subasta = "Finalizada";
+            }
+
+            _context.SaveChanges();
+        }
+
+        public void DeterminarGanadores()
+        {
+            var articulosFinalizados = _context.articulos
+                .Where(a => a.estado_subasta == "Finalizada")
+                .ToList();
+
+            foreach (var articulo in articulosFinalizados)
+            {
+                var pujasDelArticulo = _context.pujas
+                    .Where(p => p.articulo_id == articulo.Id)
+                    .OrderByDescending(p => p.monto)
+                    .ThenBy(p => p.fecha_puja)
+                    .ToList();
+
+                if (pujasDelArticulo.Any())
+                {
+                    var pujaGanadora = pujasDelArticulo.First();
+                    pujaGanadora.estado_pujas = "Ganador";
+
+                    foreach (var puja in pujasDelArticulo.Skip(1))
+                    {
+                        puja.estado_pujas = "No ganador";
+                    }
+
+                    articulo.estado_subasta = "Vendido";
+                }
+                else
+                {
+                    articulo.estado_subasta = "No vendido";
+                }
+            }
+
+            _context.SaveChanges();
+        }
+
+
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
