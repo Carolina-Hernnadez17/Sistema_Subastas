466c7a40d2bd939c57f1315fb60a22d01eef8521
Rebeca Margarita Cruz Polanco <rebeca.cruz@catolica.edu.sv>
2025-05-27 21:00:14 -0600

    Cambios en SesionActiva

---
+++ Sistema_Subastas/Atributo/SesionActiva.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sistema_Subastas.Atributo
+{
+    public class SesionActiva : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var session = context.HttpContext.Session;
+            var usuarioId = session.GetInt32("id_usuario");
+
+            var controller = context.RouteData.Values["controller"]?.ToString();
+            var action = context.RouteData.Values["action"]?.ToString();
+
+            if (usuarioId == null && !(controller == "Usuarios" && action == "Login"))
+            {
+                context.Result = new RedirectToActionResult("Login", "Usuarios", null);
+            }
+            base.OnActionExecuting(context);
+        }
+    }
+}
